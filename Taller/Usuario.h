a94f3e8e2d149eb7ffde063d711b34cae2b055d1
Valentina <valentina.lopez02@alumnos.ucn.cl>
2024-09-12 19:41:46 -0300

    clase usuario

---
+++ Taller/Usuario.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+class Usuario
+{
+private:
+    String nombre;
+    int id;
+
+public:
+    void prestarMaterial;
+    void devolverMateral;
+    void mostrarMaterialesPrestados;
+}
