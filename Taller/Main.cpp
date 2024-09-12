2087c7af777bef39113db77e24f8651608f6cb48
CL3P3 <165433361+Clerians@users.noreply.github.com>
2024-09-12 19:32:50 -0300

    menu

--- Taller/Main.cpp
+++ Taller/Main.cpp
@@ -1,6 +1,62 @@
 #include <iostream>
 
+using namespace System;
+using namespace std;
+
+void menu(){
+    cout << "Biblioteca Digital Sim" << endl;
+    cout << "1. Agregar Material a la Biblioteca" << endl;
+    cout << "2. Mostrar Información de los Materiales" << endl;
+    cout << "3. Buscar Material" << endl;
+    cout << "4. Prestar y Devolver Material" << endl;
+    cout << "5. Gestión de Usuarios" << endl;
+    cout << "0. SALIR " << endl;
+    cout << "Seleccione una opción: " << endl;
+
+}
+
 int main() {
-    std::cout << "Hola, Mundo! wuajajajajawuuwuwuwuw" << std::endl;
+
+    int op;
+
+    do{
+         menu();
+         cin >> op;
+
+        switch(op){
+            case 1:
+            cout << ": )" << endl;
+            break;
+
+            case 2:
+            cout << ":p" << endl;
+            break;
+
+            case 3:
+            cout << ":3" << endl;
+            break;
+
+            case 4:
+            cout << ":o" << endl;
+            break;
+
+            case 5:
+            cout << ":()" << endl;
+            break;
+
+            case 0:
+            cout << "Esta saliendo del programa..." << endl;
+            break;
+
+            default:
+            cout << "Opcion no valida " << endl;
+            break;
+        } 
+    System("cls");
+
+    }while(op != 0 );
+
+    cout << "Salio del programa " << endl;
+
     return 0;
 }
