4620e6b0eab5411b944065e9cb6d243adb49bb73
CL3P3 <165433361+Clerians@users.noreply.github.com>
2024-09-18 00:12:27 -0300

    Update Revista.cpp
    ahora si

--- Taller/Revista.cpp
+++ Taller/Revista.cpp
@@ -4,23 +4,24 @@
 
 using namespace std;
 
-Revista :: Revista(nombre,isbn,autor,prestado,numeroEdicion,mesPublicacion){
-    this -> numeroEdicion = numeroEdicion;
-    this -> mesPublicacion = mesPublicacion;
+Revista::Revista(string nombre, int isbn, string autor, bool prestado, int numeroEdicion, string mesPublicacion)
+    : MaterialBibliografico(nombre, isbn, autor, prestado) {
+    this->numeroEdicion = numeroEdicion;
+    this->mesPublicacion = mesPublicacion;
 }
 
-string Revista :: getNumeroEdicion(){return numeroEdicion;}
-string Revista :: getMesPublicacion(){return mesPublicacion;}
+int Revista::getNumeroEdicion() { return numeroEdicion; }
+string Revista::getMesPublicacion() { return mesPublicacion; }
 
-void Revista :: setNumeroEdicion(string numeroEdicion){this -> numeroEdicion = numeroEdicion;}
-void Revista :: setMesPublicacion(string mesPublicacion){this -> mesPublicacion = mesPublicacion;}
+void Revista::setNumeroEdicion(int numeroEdicion) { this->numeroEdicion = numeroEdicion; }
+void Revista::setMesPublicacion(string mesPublicacion) { this->mesPublicacion = mesPublicacion; }
 
-void Revista :: mostrarInformacion(){
-    MaterialBibliografico :: mostrarInformacion();
-    cout << "Numero de edicion: " << fechaDePublicacion << endl;
+void Revista::mostrarInformacion() {
+    MaterialBibliografico::mostrarInformacion();
+    cout << "Numero de edicion: " << numeroEdicion << endl;
     cout << "Mes de publicacion: " << mesPublicacion << endl;
 }
 
-Revista :: ~Revista(){
-    cout<< "Se elimina el material" << this -> nombre << " , " << this -> isbn << endl;
+Revista::~Revista() {
+    cout << "Se elimina el material " << this->getNombre() << " , " << this->getIsbn() << endl;
 }
