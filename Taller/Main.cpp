ef65444044ff47c26b770a547f3a199e4aba3b1b
CL3P3 <165433361+Clerians@users.noreply.github.com>
2024-09-18 01:02:48 -0300

    Main nuevo
    aaaaa

--- Taller/Main.cpp
+++ Taller/Main.cpp
@@ -1,3 +1,7 @@
+#include "Biblioteca.h"
+#include "Usuario.h"
+#include "Libro.h"
+#include "Revista.h"
 #include <iostream>
 
 using namespace std;
@@ -14,8 +18,75 @@ void menu(){
 
 }
 
+void convertirMinusculas(string &str) {
+    for (char &c : str) {
+        c = tolower(c); 
+    }
+}  
+
+void agregar(Biblioteca& biblioteca){
+    string tipo;
+    string nombre, autor;
+    int isbn;
+    cout << "Seleccione tipo de material (Libro o Revista): " << endl;
+    cin >> tipo; 
+    
+    convertirMinusculas(tipo);
+    
+    if(tipo == "libro"){
+        string fechaPublicacion, resumen;
+        cout << "Nombre: ";
+        cin >> nombre;
+        cout << "ISBN: ";
+        cin >> isbn;
+        cout << "Autor: ";
+        cin >> autor;
+        cout << "Fecha de Publicación: ";
+        cin >> fechaPublicacion;
+        cout << "Resumen: ";
+        cin >> resumen;
+        biblioteca.agregarMaterial(new Libro(nombre, isbn, autor, false, fechaPublicacion, resumen));
+    }
+    else if(tipo == "revista"){
+        
+        string mesPublicacion;
+        int numeroEdicion;
+        cout << "Nombre: ";
+        cin >> nombre;
+        cout << "ISBN: ";
+        cin >> isbn;
+        cout << "Autor: ";
+        cin >> autor;
+        cout << "Número de Edición: ";
+        cin >> numeroEdicion;
+        cout << "Mes de Publicación: ";
+        cin >> mesPublicacion;
+        biblioteca.agregarMaterial(new Revista(nombre, isbn, autor, false, numeroEdicion, mesPublicacion));
+    }else{
+        cout << "Tipo de material no válido. Por favor, ingrese 'Libro' o 'Revista'." << endl;
+    }
+}
+    
+void buscarMaterial(Biblioteca& biblioteca){
+    string dato;
+    cout<< " Ingrese el nombre del material o su autor:"<< endl;
+    cin >> dato;
+    convertirMinusculas(dato);
+    
+    MaterialBibliografico* a = biblioteca.buscarMaterial(dato);
+    
+    if(a != nullptr){
+       a->mostrarInformacion();
+    }else{
+        cout << "Material no encontrado" << endl;
+    }
+}
+    
 int main() {
 
+    Biblioteca biblioteca;
+    Usuario usuario1("Juan Perez", 1);
+    
     int op;
 
     do{
@@ -24,15 +95,14 @@ int main() {
 
         switch(op){
             case 1:
-            cout << ": )" << endl;
+            agregar(biblioteca);
             break;
-
             case 2:
-            cout << ":p" << endl;
+            biblioteca.mostrarMaterial();
             break;
 
             case 3:
-            cout << ":3" << endl;
+            buscarMaterial(biblioteca);
             break;
 
             case 4:
