ccc036db7ce1666f6fc7ad006ba133365ed61dc9
CL3P3 <165433361+Clerians@users.noreply.github.com>
2024-09-19 01:57:45 -0300

    nuevo main

--- Taller/Main.cpp
+++ Taller/Main.cpp
@@ -3,18 +3,21 @@
 #include "Libro.h"
 #include "Revista.h"
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 void menu(){
-    cout << "Biblioteca Digital Sim" << endl;
+    cout << "--------------------------------------------------------" << endl;
+    cout << "Biblioteca Digital" << endl;
     cout << "1. Agregar Material a la Biblioteca" << endl;
     cout << "2. Mostrar Información de los Materiales" << endl;
     cout << "3. Buscar Material" << endl;
     cout << "4. Prestar y Devolver Material" << endl;
-    cout << "5. Gestión de Usuarios" << endl;
+    cout << "5. Gestión de Usuarios(crear, buscar o eliminar usuario)" << endl;
     cout << "0. SALIR " << endl;
     cout << "Seleccione una opción: " << endl;
+    cout << "--------------------------------------------------------" << endl;
 
 }
 
@@ -82,16 +85,82 @@ void buscarMaterial(Biblioteca& biblioteca){
     }
 }
     
+void gestionUsuarios(Biblioteca& biblioteca){
+    int op;
+    cout << "1. Crear Usuario" << endl;
+    cout << "2. Buscar Usuario" << endl;
+    cout << "3. Eliminar Usuario" << endl;
+    cout << "Seleccione una opción: ";
+    cin >> op;
+
+    string nombre;
+    int id;
+
+    switch(op) {
+        case 1: {
+            cout << "Ingrese su nombre (nombre, apellido con espacios y minúsculas): ";
+            cin.ignore();  // Ignorar el salto de línea restante en el buffer
+            getline(cin, nombre);
+            convertirMinusculas(nombre);
+
+            cout << "Ingrese su rut (sin puntos ni guion): ";
+            cin >> id;
+
+            biblioteca.agregarUsuario(new Usuario(nombre, id));
+            cout << "Usuario creado exitosamente." << endl;
+            break;
+        }
+        case 2: {
+            cout << "Ingrese su nombre (nombre, apellido con espacios y minúsculas): ";
+            cin.ignore();  // Ignorar el salto de línea restante en el buffer
+            getline(cin, nombre);
+            convertirMinusculas(nombre);
+
+            cout << "Ingrese su rut (sin puntos ni guion): ";
+            cin >> id;
+
+            Usuario* encontrado = biblioteca.buscarUsuario(nombre, id);
+            if (encontrado != nullptr) {
+                encontrado->mostrarMaterialesPrestados();
+            } else {
+                cout << "Usuario no encontrado o datos incorrectos." << endl;
+            }
+            break;
+        }
+        case 3: {
+            cout << "Ingrese su nombre (nombre, apellido con espacios y minúsculas): ";
+            cin.ignore();  // Ignorar el salto de línea restante en el buffer
+            getline(cin, nombre);
+            convertirMinusculas(nombre);
+
+            cout << "Ingrese su rut (sin puntos ni guion): ";
+            cin >> id;
+
+            if (biblioteca.eliminarUsuario(nombre, id)) {
+                cout << "...." << endl;
+            } else {
+                cout << "Error: Usuario no encontrado." << endl;
+            }
+            break;
+        }
+        default: {
+            cout << "Opción no válida. Por favor, seleccione una opción válida." << endl;
+            break;
+        }
+    }
+}
+    
 int main() {
 
     Biblioteca biblioteca;
-    Usuario usuario1("Juan Perez", 1);
-    
+    biblioteca.agregarUsuario(new Usuario("juan perez", 1234));
+
     int op;
 
     do{
          menu();
          cin >> op;
+         cout << "--------------------------------------------------------" << endl;
 
         switch(op){
             case 1:
@@ -106,11 +175,11 @@ int main() {
             break;
 
             case 4:
-            cout << ":o" << endl;
+            cout << ":P" << endl;
             break;
 
             case 5:
-            cout << ":()" << endl;
+            gestionUsuarios(biblioteca);
             break;
 
             case 0:
