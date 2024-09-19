2aa9917ca7f5a9756fcc8fa2c4f6d024e5fe5e6b
CL3P3 <165433361+Clerians@users.noreply.github.com>
2024-09-19 01:54:28 -0300

    Update Biblioteca.h

--- Taller/Biblioteca.h
+++ Taller/Biblioteca.h
@@ -5,6 +5,7 @@
 #include "MaterialBibliografico.h"
 #include "Libro.h"
 #include "Revista.h"
+#include "Usuario.h"
 
 using namespace std;
 
@@ -12,13 +13,19 @@ class Biblioteca{
     private: 
         MaterialBibliografico* biblioteca[100];
         int cantidadMateriales;
+        
+        Usuario* usuarios[100];
+        int cantidadUsuarios;   
 
     public:
         Biblioteca();
         void agregarMaterial(MaterialBibliografico*);
         void mostrarMaterial();
         MaterialBibliografico* buscarMaterial(string);
+        
+        Usuario* buscarUsuario(string nombre, int id); 
+        bool eliminarUsuario(string nombre, int id);   
+        void agregarUsuario(Usuario* usuario);
+        
         ~Biblioteca();
 };
-
-
